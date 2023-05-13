4c4fe76f7e81621dcaf9bd7507ab21dc77d18eb6
Uttu32 <utkarshksharm7878@gmail.com>
2023-05-13 21:21:23 +0530

    Task added

---
+++ src/Recoil/Atom/atom.jsx
@@ -0,0 +1,6 @@
+import {atom} from "recoil";
+
+export const ListData = atom ({
+    key:"ListData",
+    default:[]
+})
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -3,63 +3,98 @@ import AddIcon from "@mui/icons-material/Add";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import RollerShadesClosedOutlinedIcon from "@mui/icons-material/RollerShadesClosedOutlined";
 import CloseIcon from "@mui/icons-material/Close";
+import DeleteIcon from '@mui/icons-material/Delete';
 import Styles from "./List.module.css";
 import Task from "../Task/Task";
+import { ListData } from "../../Recoil/Atom/atom";
+import { useRecoilState } from "recoil";
 
 const List = () => {
-  const [listName, setListName] = useState("To do");
   const [addItem, setAddItem] = useState(false);
+  const [listData, setListData] = useRecoilState(ListData);
+  const [inputVisible, setInputVisible] = useState(false);
 
   function handleAddButton() {
     setAddItem(!addItem);
   }
 
-  return (
-    <div className={Styles.mainCard}>
-      <div className={Styles.Upper}>
-        <div>
-          <input
-            className={Styles.ListName}
-            type="text"
-            value={listName}
-            onChange={(e) => setListName(e.target.value)}
-          />
-        </div>
-        <div>
-          <MoreHorizIcon fontSize="small" color="disabled" />
-        </div>
-      </div>
+  function handleDelete(Id){
+    let input = [...listData];
+    input = input.filter((ele) => ele.id !== Id )
+    setListData(input);
+  }
 
-      {addItem ? (
-        <div className={Styles.main}>
-          <Task />
-          <div className={Styles.toggle}>
-            <div className={Styles.buttonclose}>
-              <button>Add Card</button>
-              <CloseIcon onClick={handleAddButton} />
-            </div>
-            <div style={{ cursor: "pointer" }}>
-              <MoreHorizIcon fontSize="large" color="disabled" />
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className={Styles.Addbtn}>
-          <div>
-            <button onClick={handleAddButton} className={Styles.addButton}>
-              <AddIcon
-                sx={{ marginBottom: "-5px", paddingRight: "4px" }}
-                fontSize="small"
-                color="#B7BCC7"
+  function handleChange(e,Id){
+    let input = [...listData];
+    let index = input.findIndex((ele)=> ele.id == Id);
+    let current = {...input[index]};
+    current.listName = e.target.value;
+    input[index] = current;
+    setListData(input);
+  }
+
+  return (
+    <div style={{display:"flex"}}>
+      {listData.map((ele) => {
+        return (
+          <div className={Styles.mainCard}>
+            <div className={Styles.Upper}>
+              {inputVisible ?
+              <form onSubmit={()=>setInputVisible(false)}>
+              <input
+                className={Styles.ListName}
+                type="text"
+                placeholder={ele.listName}
+                onChange={(e)=>handleChange(e,ele.id)}
               />
-              Add a card
-            </button>
+              <DeleteIcon
+              onClick={()=>handleDelete(ele.id)}
+              fontSize="small" />
+              </form>
+              : 
+              <p style={{width:"100%"}} onClick={()=>setInputVisible(true)}>{ele.listName}</p> 
+            
+              }
+</div>
+            {addItem ? (
+              <div className={Styles.main}>
+                <Task />
+                <div className={Styles.toggle}>
+                  <div className={Styles.buttonclose}>
+                    <button>Add Card</button>
+                    <CloseIcon onClick={handleAddButton} />
+                  </div>
+                  <div style={{ cursor: "pointer" }}>
+                    <MoreHorizIcon fontSize="large" color="disabled" />
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className={Styles.Addbtn}>
+                <div>
+                  <button
+                    onClick={handleAddButton}
+                    className={Styles.addButton}
+                  >
+                    <AddIcon
+                      sx={{ marginBottom: "-5px", paddingRight: "4px" }}
+                      fontSize="small"
+                      color="#B7BCC7"
+                    />
+                    Add a card
+                  </button>
+                </div>
+                <div>
+                  <RollerShadesClosedOutlinedIcon
+                    fontSize="small"
+                    color="disabled"
+                  />
+                </div>
+              </div>
+            )}
           </div>
-          <div>
-            <RollerShadesClosedOutlinedIcon fontSize="small" color="disabled" />
-          </div>
-        </div>
-      )}
+        );
+      })}
     </div>
   );
 };
